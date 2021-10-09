f33f69096845357ff0fbaa7bf2236dc898e7c2d5
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-10-09 15:28:49 +0530

    Enabled cors in server

--- server/app.js
+++ server/app.js
@@ -23,5 +23,17 @@ mongoose
 
 
 
+// configuring middleware for cors
+app.use(cors());
+
+
+// serving the files from the server
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // listening the server
 app.listen(port, () => console.log(`Server is running at port ${port}`));
