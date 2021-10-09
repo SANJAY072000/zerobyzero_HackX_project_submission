2c33aa975ee0a31a5f33a0debd850bcd569fc3a2
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-10-09 15:53:20 +0530

    Added the route to add, edit and delete doctor details

--- server/routes/api/doctors.js
+++ server/routes/api/doctors.js
@@ -25,6 +25,55 @@ router.get(
   }
 );
 
+/*
+@type - GET
+@route - /api/doctor/listOfDoctors
+@desc - a route to get all doctors of the hospital
+@access - PRIVATE
+*/
+router.get(
+  "/listOfDoctors",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Doctor.find({ user: req.user._id })
+      .then((doctor) => res.status(200).json(doctor))
+      .catch((err) => console.log(err));
+  }
+);
+
+/*
+@type - POST
+@route - /api/doctor/addDoctor
+@desc - a route to add doctors to the hospital
+@access - PRIVATE
+*/
+router.post(
+  "/addDoctor",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const doctorValues = {};
+    doctorValues.user = req.user._id;
+    if (req.body.name) doctorValues.name = req.body.name;
+    if (req.body.img) doctorValues.img = req.body.img;
+    if (req.body.domain) doctorValues.domain = req.body.domain;
+    Hospital.findOne({ _id: req.user._id })
+      .then((hospital) => {
+        hospital.credits--;
+        hospital
+          .save()
+          .then((hospital) => {
+            new Doctor(doctorValues)
+              .save()
+              .then((doctor) => res.status(200).json(doctor))
+              .catch((err) => console.log(err));
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
+);
+
+
 
 // exporting the routes
 module.exports = router;
