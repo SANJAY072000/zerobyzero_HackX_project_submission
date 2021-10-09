a8768c9b36c2495f06c35c2600581536129d2d5d
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-10-09 16:04:06 +0530

    Get the route to fetch location of all registered clinics

--- server/routes/api/appointments.js
+++ server/routes/api/appointments.js
@@ -8,6 +8,29 @@ const express = require("express"),
 const Hospital = require("../../models/Hospital"),
   Doctor = require("../../models/Doctor");
 
+/*
+@type - GET
+@route - /api/appointment/locdom
+@desc - a route to get location of hospitals/clinics and domain of doctors
+@access - PUBLIC
+*/
+router.get("/locdom", (req, res) => {
+  let loc = [],
+    dom = [];
+  Hospital.find()
+    .then((hospital) => {
+      hospital.forEach((a) => loc.push(a.hLocation));
+      Doctor.find()
+        .then((doctor) => {
+          doctor.forEach((a) => dom.push(a.domain));
+          return res.status(200).json({ loc: loc, dom: dom });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
+
+
 
 
 // exporting the routes
