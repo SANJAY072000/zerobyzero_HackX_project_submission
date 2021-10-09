923c91831c045411780a230acdf263aeb9d36cce
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2021-10-09 16:16:03 +0530

    Placing appointments route fixed

--- server/routes/api/appointments.js
+++ server/routes/api/appointments.js
@@ -30,7 +30,57 @@ router.get("/locdom", (req, res) => {
     .catch((err) => console.log(err));
 });
 
+/*
+@type - POST
+@route - /api/appointment/docs
+@desc - a route to get the doctors according to the filters
+@access - PUBLIC
+*/
+router.post("/docs", (req, res) => {
+  let dol = [];
+  Hospital.find({ hLocation: req.body.loc })
+    .then((hospital) => {
+      Doctor.find({ domain: req.body.dom })
+        .then((doctor) => {
+          hospital.forEach((a) => {
+            doctor.forEach((b) => {
+              if (a._id.toString() === b.user.toString()) dol.push(b);
+            });
+          });
+          return res.status(200).json({ dol });
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
 
+/*
+@type - GET
+@route - /api/appointment/book-:dcid-:ptemid
+@desc - a route to book an appointment with the doctor
+@access - PUBLIC
+*/
+router.get("/book-:dcid-:ptemid", (req, res) => {
+  Doctor.findOne({ _id: req.params.dcid })
+    .then((doctor) => {
+      doctor.totalToken++;
+      Hospital.findOne({ _id: doctor.user })
+        .then((hospital) => {
+          doctor
+            .save()
+            .then((doctor) => {
+              return res
+                .status(200)
+                .json({
+                  appointmentSuccess: "Appointment Placed Successfully",
+                });
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
+});
 
 
 // exporting the routes
